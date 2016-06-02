eff15f71837de0b31ef355968b5544b3fcd80dbe
Setoge <moon13sun@163.com>
2016-06-02 16:00:52 +0800

    10-弹性附着,手柄重绘方案2
    方案二: KVO (黑魔法)方式解决手柄不能重绘问题,本质上是通过通知实现

--- DynamicBehaviors/Classes/Views/STSpringView.m
+++ DynamicBehaviors/Classes/Views/STSpringView.m
@@ -22,14 +22,32 @@
         _attachment.frequency = 0.5;
         
         // 方案1:
-        CADisplayLink *link = [CADisplayLink displayLinkWithTarget:self selector:@selector(refresh)];
-        [link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
-
-
+//        CADisplayLink *link = [CADisplayLink displayLinkWithTarget:self selector:@selector(refresh)];
+//        [link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+        
+        // 方案2:
+        [_boxView addObserver:self forKeyPath:@"center" options:NSKeyValueObservingOptionOld context:nil];
+        
     }
     return self;
 }
 
+
+
+// KVO的监听方法
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    
+    [self setNeedsDisplay];
+    
+}
+
+#pragma mark - 2.移除监听者
+- (void)dealloc {
+    
+    [_boxView removeObserver:self forKeyPath:@"center"];
+}
+
+#pragma mark - 1.定时器监听方法
 - (void)refresh{
     
     [self setNeedsDisplay];
